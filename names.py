6b5e252aa82f8e92ad0e25b1903b8b461b23e86d
dheredia19 <dheredia19@users.noreply.github.com>
2018-09-12 08:11:36 -0400

    Added comments

--- names.py
+++ names.py
@@ -4,14 +4,14 @@ print("Hello ", end='') # Begin greeting string
 
 for x in range(1, len(sys.argv)): # Assess number of arguments
 	if x == len(sys.argv)-1: # If parser reaches the last argument
-		if len(sys.argv) != 2:
+		if len(sys.argv) != 2: # No conjunction needed for one name, catch exception
 			print("and ", end='') # Print an "and"
 	print(sys.argv[x], end='') # Get the name from argument
 	if x == len(sys.argv)-1: # If parser reaches the last argument
 		print("!", end='') # Print an exclamation point
 		quit() # Stop execution
 	else: # If parser does not reach the last argument
-		if len(sys.argv) != 3:
+		if len(sys.argv) != 3: # No comma needed for two names, catch exception
 			print(", ", end='') # Print a comma
-		else:
+		else: # Space still needed for names
 			print(" ", end='') # Print a comma
