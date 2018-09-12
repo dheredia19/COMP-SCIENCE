606dbbd3ec79fbe6ed44460e6a631544feb947b0
dheredia19 <dheredia19@users.noreply.github.com>
2018-09-12 08:08:44 -0400

    Changed grammar choices

--- names.py
+++ names.py
@@ -1,13 +1,17 @@
-import sys
+import sys # Call system functions
 
-print("Hello ", end='')
+print("Hello ", end='') # Begin greeting string
 
-for x in range(1, len(sys.argv)):
-	if x == len(sys.argv)-1:
-		print("and ", end='')
-	print(sys.argv[x], end='')
-	if x == len(sys.argv)-1:
-		print("!", end='')
-		quit()
-	else:
-		print(", ", end='')
+for x in range(1, len(sys.argv)): # Assess number of arguments
+	if x == len(sys.argv)-1: # If parser reaches the last argument
+		if len(sys.argv) != 2:
+			print("and ", end='') # Print an "and"
+	print(sys.argv[x], end='') # Get the name from argument
+	if x == len(sys.argv)-1: # If parser reaches the last argument
+		print("!", end='') # Print an exclamation point
+		quit() # Stop execution
+	else: # If parser does not reach the last argument
+		if len(sys.argv) != 3:
+			print(", ", end='') # Print a comma
+		else:
+			print(" ", end='') # Print a comma
