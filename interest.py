116e372bd7c789cf0df601a48679d5862e5ab95d
dheredia19 <dheredia19@users.noreply.github.com>
2018-09-14 11:26:35 -0400

    Uploaded interest

---
+++ interest.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+print("I will continuously compound your interest until your pockets swell with cash!")
+
+P = sys.argv[1]
+r = sys.argv[2]
+t = sys.argv[3]
+
+print("Detected arguments:")
+print("Principal amount: $" + P)
+print("Annual interest rate: " + r)
+print("Time: " + t + " years")
+
+P = float(P)
+r = float(r)
+t = float(t)
+
+M = P*math.e**(r*t)
+
+M = str(M)
+
+print("Resulting money: $" + M + "!")
