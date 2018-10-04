17a57c51ce6ecddf29e5cf55ae4c83ecce8637a9
dheredia19 <43074107+dheredia19@users.noreply.github.com>
2018-10-03 22:38:50 -0400

    Minesweeper Completion
    All game features added!

--- minesweeper.py
+++ minesweeper.py
@@ -2,86 +2,217 @@ import random
 
 print("This is a simple Minesweeper game, which you are destined to lose.")
 height = input("How many rows do you want? ")
-width = input("How many column do you want? ")
+width = input("How many columns do you want? ")
+bombs = input("How many bombs do you want? ")
 
-w = int(width)
-h = int(height)
+# Catch for non-number user inputs
+try:
+	# Set params for grid to exceed printed range
+	w = int(width)+2
+	h = int(height)+2
+	b = int(bombs)
+except ValueError:
+	print("Can you try typing in regular numbers next time?")
+	quit()
 
-matrix = [[0]*w for i in range(h)]
+# Initialize game grids
+solved = [[int("0")]*w for i in range(h)]
+covered = [["■"]*w for i in range(h)]
 
-def display():
+# Print revealed game screen for computations
+def showing():
 	global x
 	global y
-	global matrix
-	for x in range(h):
-		for y in range(w):
-			print(matrix[x][y], end = ' ')
+	global solved
+	# Set params for grid to exceed printed range
+	for x in range(1,h-1):
+		for y in range(1,w-1):
+			print(solved[x][y], end = ' ')
 		print()
+	print()
 
-display()
+# Print covered game screen for user
+def hiding():
+	global x
+	global y
+	global covered
+	# Set params for grid to exceed printed range
+	for x in range(1,h-1):
+		for y in range(1,w-1):
+			print(covered[x][y], end = ' ')
+		print()
+	print()
 
+# Restrict number of spaces
+if w < 10 or h < 10:
+	print("What kind of Minesweeper do you play? This ain't supposed to be no walk in the park!")
+	quit()
+
+# Restrict number of bombs
 maxbombs = w*h
-bombs = input("How many bombs do you want? ")
-b = int(bombs)
-
 if b > maxbombs:
 	print("Yikes! You must be a terrorist, because that's more bombs than your grid can support!")
 	quit()
 
+# Reset game variables
 result = None
+correct = 0
 
 def bomber():
 	global result
-	findX = random.randint(0,h)
-	findY = random.randint(0,w)
-	print("(" + str(findX) + "," + str(findY) + ")")
-	matrix[findX][findY] = "*"
+	# Set params for grid to exceed printed range
+	findX = random.randint(1,h-2)
+	findY = random.randint(1,w-2)
+	# print("(" + str(findX) + "," + str(findY) + ")")
+	solved[findX][findY] = "*"
 
+# Display initial hidden grid
+print()
+print("Let's start the game!")
+hiding()
+
+# Recalculate bomb grid until bomber function succeeds
 while result is None:
 	try:
 		for x in range(b):
 			bomber()
 		result = True
 	except:
-		matrix = [[0]*w for i in range(h)]
+		solved = [[0]*w for i in range(h)]
 
-display()
+# First run of game loop to calculate bombs around selection
+try:
+	while True:
+		row = input("Which row will you like to select? ")
+		column = input("Which column will you like to select? ")
+		# Catch for inputs that are not numbers
+		try:
+			r = int(row)
+			c = int(column)
+		except ValueError:
+			flag = input("Since you didn't type in numbers, would you like to place a flag? ")
+			# Prompt user for flag coordinates
+			if "y" in flag:
+				row = input("Which row will you like to select? ")
+				column = input("Which column will you like to select? ")
+				# Catch for inputs that are not numbers
+				try:
+					r = int(row)
+					c = int(column)
+					# Catch for numbers that are too big
+					try:
+						covered[r][c] = "►"
+						hiding()
+					except IndexError:
+						print("C'mon! Your inputs are out of the range!")
+						continue
+				except ValueError:
+					print("Can you try typing in regular numbers, then?")
+					continue
+			print("Can you try typing in regular numbers, then?")
+			continue
+		# Catch for numbers that are too big
+		try:
+			covered[r][c] = str(solved[r][c])
+		except IndexError:
+			print("C'mon! Your inputs are out of the range!")
+			continue
+		hiding()
+		break
+except KeyboardInterrupt:
+	print()
+	print("Ha! You fool! I knew you would chicken out. Now get lost!")
+	quit()
 
+# Calculate bomb counts in surrounding spaces
 for x in range(h):
 	for y in range(w):
-		if matrix[x][y] == "*":
+		if solved[x][y] == "*":
 			try:
-				matrix[x-1][y-1] += 1
+				solved[x-1][y-1] += 1
 			except:
 				pass
 			try:
-				matrix[x-1][y] += 1
+				solved[x-1][y] += 1
 			except:
 				pass
 			try:
-				matrix[x-1][y+1] += 1
+				solved[x-1][y+1] += 1
 			except:
 				pass
 			try:
-				matrix[x][y-1] += 1
+				solved[x][y-1] += 1
 			except:
 				pass
 			try:
-				matrix[x][y+1] += 1
+				solved[x][y+1] += 1
 			except:
 				pass
 			try:
-				matrix[x+1][y-1] += 1
+				solved[x+1][y-1] += 1
 			except:
 				pass
 			try:
-				matrix[x+1][y] += 1
+				solved[x+1][y] += 1
 			except:
 				pass
 			try:
-				matrix[x+1][y+1] += 1
+				solved[x+1][y+1] += 1
 			except:
 				pass
 
-print()
-display()
+# Game loop to catch Ctrl-C
+try:
+	while True:
+		row = input("Which row will you like to select? ")
+		column = input("Which column will you like to select? ")
+		# Catch for inputs that are not numbers
+		try:
+			r = int(row)
+			c = int(column)
+		except ValueError:
+			flag = input("Since you didn't type in numbers, would you like to place a flag? ")
+			# Prompt user for flag coordinates
+			if "y" in flag:
+				row = input("Which row will you like to select? ")
+				column = input("Which column will you like to select? ")
+				# Catch for inputs that are not numbers
+				try:
+					r = int(row)
+					c = int(column)
+					# Catch for numbers that are too big
+					try:
+						covered[r][c] = "►"
+						# Check if correct flag placement
+						if "*" in str(solved[r][c]):
+							correct += 1
+						# Win sequence
+						if correct == b:
+							print("Wow! You actually won the game! Now get lost!")
+							quit()
+						hiding()
+					except IndexError:
+						print("C'mon! Your inputs are out of the range!")
+						continue
+				except ValueError:
+					print("Can you try typing in regular numbers, then?")
+					continue
+			else:
+				print("Can you try typing in regular numbers, then?")
+			continue
+		# Catch for numbers that are too big
+		try:
+			covered[r][c] = str(solved[r][c])
+		except IndexError:
+			print("C'mon! Your inputs are out of the range!")
+			continue
+		# Jump out of loop if bomb uncovered
+		if "*" in covered[r][c]:
+			print("Ha! You fool! You landed on a bomb. Now get lost!")
+			showing()
+			quit()
+		else:
+			hiding()
+except KeyboardInterrupt:
+	print()
+	print("Ha! You fool! I knew you would chicken out. Now get lost!")
